3a0d5bf95f5a0a7f1688ef4cf24bfed5ef5ff298
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-09-13 15:20:06 +0530

    added some route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
+
+const Signup = lazy(() => import("./components/auth/Signup"));
+const Login = lazy(() => import("./components/auth/Login"));
+const Reset = lazy(() => import("./components/auth/Reset"));
+const Forget = lazy(() => import("./components/auth/Forget"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forget" element={<Forget />} />
+        <Route path="/reset" element={<Reset />} />
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/components/shared/LanguageChange.jsx
+++ src/components/shared/LanguageChange.jsx
@@ -1,9 +1,9 @@
 import { Combobox, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Fragment, useContext, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { TranslateContext } from "../../context/TranslateContext";
 
-const people = [
+const countries = [
   {
     nativeName: "English",
     url: "https://www.worldometers.info//img/flags/small/tn_us-flag.gif",
@@ -11,7 +11,7 @@ const people = [
     tag: "en",
   },
   {
-    nativeName: "Deutsch",
+    nativeName: "Dutch",
     url: "https://www.worldometers.info//img/flags/small/tn_be-flag.gif",
     name: "De",
     tag: "de",
@@ -25,9 +25,16 @@ const people = [
 ];
 
 const LanguageChange = () => {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState([]);
   const { translation } = useContext(TranslateContext);
   const { i18n } = translation;
+
+  useEffect(() => {
+    const lang = countries.filter((el) => el.tag === i18n?.language);
+    setSelected(lang[0]);
+  }, []);
+
+  console.log("selected", selected?.name);
   return (
     <Combobox
       value={selected}
@@ -40,18 +47,13 @@ const LanguageChange = () => {
         <div className="relative cursor-default overflow-hidden rounded-md bg-white text-left shadow-md sm:text-sm">
           <Combobox.Input
             className="border-none py-2 pl-2 text-sm leading-5 text-gray-900 "
-            displayValue={(person) => person.name}
+            displayValue={() => selected?.name}
             disabled
           />
-          {/* <img
-            src={"https://www.worldometers.info//img/flags/small/tn_in-flag.gif"}
-            alt=""
-            sizes=""
-            srcset={"https://www.worldometers.info//img/flags/small/tn_in-flag.gif"}
-            className="w-5 h-5"
-          /> */}
+
           <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <img src={selected?.url} alt="" srcSet={selected?.url} className="h-5 w-5" />
+            <ChevronDownIcon className="h-5 w-5 px-1 text-gray-400" aria-hidden="true" />
           </Combobox.Button>
         </div>
         <Transition
@@ -61,7 +63,7 @@ const LanguageChange = () => {
           leaveTo="opacity-0"
         >
           <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {people.map((person) => (
+            {countries.map((person) => (
               <Combobox.Option
                 key={person.tag}
                 className={({ active }) =>
@@ -80,7 +82,7 @@ const LanguageChange = () => {
                       src={person?.url}
                       alt=""
                       sizes=""
-                      srcset={person?.url}
+                      srcSet={person?.url}
                       className="w-5 h-5"
                     />
                   </div>
